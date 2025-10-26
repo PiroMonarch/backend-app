7ef3a7f3df980af499d90e7435d3b1b4e31159f5
Chetan Choudhary <choudharychetan136@gmail.com>
2025-10-26 21:52:31 +0530

    Enhance MongoDB connection check
    Added a check for connectionInstance to handle potential issues during development.

--- src/db/index.js
+++ src/db/index.js
@@ -3,11 +3,15 @@
 
 import mongoose from "mongoose";    
 import { DB_NAME } from "../constants.js";
-import express from "express";
+
  
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+     //adding  an additonal  check  for  connectionInstance cause trouble in development !
+     if(!connectionInstance ||connectionInstance===undefined){
+      console.log(`the env  variable  are not accessible properly !! please check to the .env file  as  connectionInstance is  ${connectionInstance} `)
+     }
         console.log(`\n MONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`);     
     } catch (error) {
         console.log("MONGODB connection FAILED:", error);
